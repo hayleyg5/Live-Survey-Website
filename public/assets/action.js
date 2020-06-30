13d1b5ebb87abb0193b14d99477fe15d8766e2ea
Hayley Goodman <31858311+hayleyg5@users.noreply.github.com>
2020-06-30 13:51:08 -0400

    Add files via upload

---
+++ public/assets/action.js
@@ -0,0 +1,24 @@
+// Modified from Caroline Barriere's original file
+// jQuery that will "listen" to the html niceSurvey.html
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+      
+      // var item = $('form input');
+      // console.log(item.serializeArray());
+
+      $.ajax({
+        type: 'POST',
+        url: '/WorksSurvey',
+        data: $(this).serializeArray(),
+        success: function(data){
+          // do something with the data via front-end framework
+          // Make the submit button red, disabled and saying Thank you
+          $("#bb").css("background-color", "rgb(0, 102, 255)");
+          $("#bb").prop("disabled", "true");
+          $("#bb").text("Thank you!");
+        }
+      });
+      return false;
+  });
+});
