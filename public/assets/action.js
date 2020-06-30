0361fc06f9fbe3da0d03a5bd7fdfd70512bbfd89
Hayley Goodman <31858311+hayleyg5@users.noreply.github.com>
2020-06-30 14:10:35 -0400

    Update action.js

--- public/assets/action.js
+++ public/assets/action.js
@@ -1,5 +1,5 @@
 // Modified from Caroline Barriere's original file
-// jQuery that will "listen" to the html niceSurvey.html
+// jQuery that will "listen" to the html WorksSurvey.html
 $(document).ready(function(){
 
   $('form').on('submit', function(){
@@ -13,7 +13,7 @@ $(document).ready(function(){
         data: $(this).serializeArray(),
         success: function(data){
           // do something with the data via front-end framework
-          // Make the submit button red, disabled and saying Thank you
+          // Make the submit button blue, disabled and saying Thank you
           $("#bb").css("background-color", "rgb(0, 102, 255)");
           $("#bb").prop("disabled", "true");
           $("#bb").text("Thank you!");
